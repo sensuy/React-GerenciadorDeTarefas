04aee950349347dd63dfc8966c6def0dff781ecd
sensui <sensuijunior@hotmail.com>
2020-07-06 02:55:40 -0300

    interface cadastrar done

---
+++ src/cadastrar/cadastrar-tarefa.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Button, Form, Jumbotron, Modal, ModalFooter } from 'react-bootstrap';
+import { A, navigate } from 'hookrouter';
+
+
+
+function CadastrarTarefa() {
+	return (
+		<div>
+			<h3 className="text-center">Cadastrar</h3>
+			<Jumbotron>
+				<Form>
+					<Form.Group>
+						<Form.Label>Tarefa</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="Digite a tarefa"
+							minLength="5"
+							maxLength="100"
+							required />
+						<Form.Control.Feedback type="invalid">
+							A tarefa deve conter ao menos  5 caracteres
+						</Form.Control.Feedback>
+					</Form.Group>
+					<Form.Group className="text-center">
+						<Button
+							variant="success"
+							type="submit">
+							Cadastrar
+						</Button>
+						&nbsp;
+						<A href="/" className="btn btn-light">
+							Voltar
+						</A>
+					</Form.Group>
+				</Form>
+				<Modal show={false}>
+					<Modal.Header closeButton>
+						<Modal.Title>Sucesso</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						Tarefa adicionada com sucesso!
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="success">
+							Continuar
+						</Button>
+					</Modal.Footer>
+				</Modal>
+			</Jumbotron>
+
+		</div>
+	);
+}
+
+export default CadastrarTarefa;
