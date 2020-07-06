feb2658d29e2c043a0b9dac79b9cfa9e00d458c0
sensui <sensuijunior@hotmail.com>
2020-07-06 00:53:09 -0300

    project set

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import GerenciadorTarefas from './gerenciador-tarefas';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GerenciadorTarefas />,
   document.getElementById('root')
 );
 
